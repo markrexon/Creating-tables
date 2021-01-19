d6673765206c0862be7eefeadfe38fe042a634a3
markrexon <68454892+markrexon@users.noreply.github.com>
2021-01-19 23:13:55 +0530

    table construction
    It can generate the table , it is use full for studying tables, and i will ended when you enter 0 otherwice it will generate the new tables.

--- tables.c
+++ tables.c
@@ -1,3 +1,19 @@
-# Creating-tables
-//tables for 20X20
+#include<stdio.h>
+void main(){
+   int n=2,i,choice;
+   do{
+    i=1;
+    while(i<=20){
+       printf("%d X %d= %d\n",n,i,(n*i));
+       i++;
 
+    }
+    printf("do you want to continue in %d otherwise write the number",n+1);
+    scanf("%d",&choice);
+    if(choice==0){
+        break;
+    }
+    n++;
+
+   }while(1);
+}
